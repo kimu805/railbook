bde9295a1f4602f78da6d022b06b83d0b9094447
kimu805 <kimukimuok805@gmail.com>
2025-03-23 18:11:46 +0900

    delete

--- app/controllers/messages_controller.rb
+++
@@ -1,7 +0,0 @@
-class MessagesController < ApplicationController
-
-  def index
-    # @messages = Message.order(updated_at: :desc)
-    @messages = Message.where(topic: params[:topic]).order(updated_at: :desc)
-  end
-end
--- app/controllers/routes_controller.rb
+++
@@ -1,5 +0,0 @@
-class RoutesController < ApplicationController
-  def index
-    render plain: 'indexアクションが実行されました'
-  end
-end
