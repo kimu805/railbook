b5ec7d32d6a1a1a67ee6975ccf181f7823678990
kimu805 <kimukimuok805@gmail.com>
2025-03-29 12:40:11 +0900

    Update view_helper.rb

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -14,21 +14,6 @@ module ViewHelper
     datetime.strftime(format)
   end
 
-  def format_datetime(datetime, type = :datetime)
-    return "" unless datetime
-
-    case type
-    when :datetime
-      format = "%Y年%m月%d日 %H:%M:%S"
-    when :date
-      format = "%Y年%m月%d日"
-    when :time
-      format = "%H:%M:%S"
-    end
-
-    datetime.strftime(format)
-  end
-
   def list_tag(collection, prop)
     tag.ul do
       collection.each do |el|
